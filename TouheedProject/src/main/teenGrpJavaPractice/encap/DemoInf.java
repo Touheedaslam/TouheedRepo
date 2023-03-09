4ed6964eca634fc19a4b3673fa492b81439ed66b
Touheedaslam <touheed.aslam@gmail.com>
2023-03-09 16:46:20 +0530

    practiced

---
+++ TouheedProject/src/main/teenGrpJavaPractice/encap/DemoInf.java
@@ -0,0 +1,35 @@
+package encap;
+
+public interface DemoInf {
+	public void companyDetails();
+	public abstract void empDetails();
+	public abstract void projectDetails();
+
+
+	
+}
+
+class Demo implements DemoInf
+{
+
+	public void companyDetails() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void empDetails() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void projectDetails() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int DemoInf() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
